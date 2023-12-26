f5b7f2cb6fead420a51c3c313d1becc25a3c1d5b
LiuLawrence45 <liu.lawrence45@gmail.com>
2023-12-25 20:09:33 -0500

    file upload handled.

--- app.py
+++ app.py
@@ -11,38 +11,52 @@ from langchain.llms import OpenAI
 from langchain.agents import create_csv_agent
 from langchain.agents.agent_types import AgentType
 
-from langchain.chains import LLMChain, SimpleSequentialChain
-from langchain.prompts import PromptTemplate 
-
-
-## Loading PDF 
-from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import Chroma 
+#tempfile creation from input
+import tempfile
 
 
 os.environ['OPENAI_API_KEY'] = apikey
 
 st.title("Emu-GEN")
 prompt = st.text_input("Enter prompt here: ")
+
 file = st.file_uploader("Upload text conversation here (filetype .csv): ")
-agent = create_csv_agent(
-    OpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
-    "titanic.csv",
-    verbose=True,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-)
 
-title_template = PromptTemplate(
-    input_variables = ['topic'], 
+if file is not None: 
+    
+    #Creation of tempfile is necessary, since we are uploading to server 
+    with tempfile.NamedTemporaryFile(mode='w+', suffix=".csv", delete=False) as f:
+        # Convert bytes to a string before writing to the file
+        data_str = file.getvalue().decode('utf-8')
+        f.write(data_str)
+        f.flush()
 
-    template = open('Prompts/default_prompt', 'r').read()
-)
+    agent = create_csv_agent(
+        OpenAI(temperature=0),
+        f.name, 
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
+
+    if prompt: 
+        response = agent.run(prompt)
+        st.write(response)
+
+else: 
+    st.markdown("*Knowledge file must be uploaded to continue.*")
 
 
-llm = OpenAI(temperature = 0.4)
-title_chain = LLMChain(llm=llm, prompt = title_template, verbose = True)
+# title_template = PromptTemplate(
+#     input_variables = ['topic'], 
 
-if prompt: 
-    response = title_chain.run(topic = prompt)
-    st.write(response)
+#     template = open('Prompts/default_prompt', 'r').read()
+# )
+
+
+# llm = OpenAI(temperature = 0.4)
+# title_chain = LLMChain(llm=llm, prompt = title_template, verbose = True)
+
+# if prompt: 
+#     response = title_chain.run(topic = prompt)
+#     st.write(response)
 
